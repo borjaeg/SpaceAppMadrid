b955bd04637854d23f5fece592c140f48334a27d
Borja Espejo Garcia <borja3790@gmail.com>
2013-04-22 18:08:18 +0200

    pipe creator modified

--- kmlbi/src/main/java/es/tony/main/PipeCreator.java
+++ kmlbi/src/main/java/es/tony/main/PipeCreator.java
@@ -23,31 +23,14 @@ public class PipeCreator {
 		ConexionBaseDatos.init();
 
 		Configurator configurator = new Configurator("postgres",
-				"mamagiorgias", "1983", "2012", "org.postgresql.Driver",
+				"password", "1983", "2012", "org.postgresql.Driver",
 				"jdbc:postgresql://localhost:5432/MeteoDB", "45", "36", "5",
 				"-9", "10", "SolarRadiation", "swv_dwn");
 		etlPipe_1 = new ETLPipe(configurator);
 
-//		configurator = new Configurator("postgres", "mamagiorgias", "1983",
-//				"2012", "org.postgresql.Driver",
-//				"jdbc:postgresql://localhost:5432/MeteoDB", "41", "39", "4",
-//				"-9", "10", "RelativeHumidity", "RH2M");
-//		etlPipe_2 = new ETLPipe(configurator);
-//
-//		configurator = new Configurator("postgres", "mamagiorgias", "1983",
-//				"2012", "org.postgresql.Driver",
-//				"jdbc:postgresql://localhost:5432/MeteoDB", "44", "42", "4",
-//				"-9", "10", "DewFrost", "DFP2M");
-//		etlPipe_3 = new ETLPipe(configurator);
-
-		etlPipe_1.start();
-//		etlPipe_2.start();
-//		etlPipe_3.start();
 
 		try {
 			etlPipe_1.join();
-//			etlPipe_2.join();
-//			etlPipe_3.join();
 		} catch (InterruptedException e) {
 			log.error(e.getStackTrace().toString());
 		}
