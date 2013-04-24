1e0a037ebc05f78c321c2959ec4da583a1d1e9d4
Alberto Alcolea <albertoalcolea@gmail.com>
2013-04-24 12:34:47 +0200

    Fixed bug in install_geogash script

--- kmlbi/install_geohash.sh
+++ kmlbi/install_geohash.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/kungfoo/geohash-java.git
 cd geohash-java
-mvn compile
-mvn package
+mvn compile && \
+mvn package && \
+cd target && \
 mvn install:install-file -Dfile=ch.hsr.geohash-1.0.6.jar -DgroupId=ch.hsr -DartifactId=ch.hsr.geohash -Dversion=1.0.6 -Dpackaging=jar
